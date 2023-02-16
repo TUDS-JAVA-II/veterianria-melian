36f8512a9dff6351c9a9b68b170a54b384602057
raffarraffa <50971821+raffarraffa@users.noreply.github.com>
2023-02-16 10:22:52 -0300

    Update veterinaria.sql

--- veterinaria.sql
+++ veterinaria.sql
@@ -1,3 +1,11 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1:3306
+-- Tiempo de generación: 16-02-2023 a las 13:09:11
+-- Versión del servidor: 10.10.2-MariaDB
+-- Versión de PHP: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -16,57 +24,90 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `cliente`
+--
+
+DROP TABLE IF EXISTS `cliente`;
+CREATE TABLE IF NOT EXISTS `cliente` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `dni` char(16) NOT NULL,
+  `apellido` varchar(50) NOT NULL,
+  `nombres` varchar(50) NOT NULL,
+  `domicilio` varchar(50) NOT NULL,
+  `tel_movil` int(11) NOT NULL,
+  `email` varchar(250) NOT NULL,
+  `creado` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`),
+  KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `especie`
+--
+
+DROP TABLE IF EXISTS `especie`;
+CREATE TABLE IF NOT EXISTS `especie` (
+  `id` int(11) NOT NULL,
+  `especie` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `historia_clinica`
+--
+
+DROP TABLE IF EXISTS `historia_clinica`;
+CREATE TABLE IF NOT EXISTS `historia_clinica` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_mascota` int(11) NOT NULL,
+  `intervencion` text NOT NULL,
+  `creado` timestamp NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_mascota` (`id_mascota`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `mascota`
 --
 
 DROP TABLE IF EXISTS `mascota`;
 CREATE TABLE IF NOT EXISTS `mascota` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `tipo` int NOT NULL,
-  `nombre` varchar(50) COLLATE utf8mb4_spanish_ci NOT NULL,
-  `raza` varchar(50) COLLATE utf8mb4_spanish_ci NOT NULL DEFAULT 'sin raza',
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_especie` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `raza` int(11) NOT NULL DEFAULT 0,
   `nacido` timestamp NOT NULL,
-  `color` varchar(50) COLLATE utf8mb4_spanish_ci NOT NULL,
-  `status` int NOT NULL DEFAULT '1',
-  `dueño` int NOT NULL,
-  `ingreso` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `color` varchar(50) NOT NULL,
+  `status` int(11) NOT NULL DEFAULT 1,
+  `id_cliente` int(11) NOT NULL,
+  `chip` int(11) NOT NULL,
+  `creado` timestamp NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`id`),
-  KEY `tipo` (`tipo`)
+  KEY `ESPECIE` (`id_especie`) USING BTREE,
+  KEY `DUENIO` (`id_cliente`),
+  KEY `raza` (`raza`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `personas`
+-- Estructura de tabla para la tabla `raza`
 --
 
-DROP TABLE IF EXISTS `personas`;
-CREATE TABLE IF NOT EXISTS `personas` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `dni` char(16) COLLATE utf8mb4_spanish_ci NOT NULL,
-  `apellido` varchar(50) COLLATE utf8mb4_spanish_ci NOT NULL,
-  `nombres` varchar(50) COLLATE utf8mb4_spanish_ci NOT NULL,
-  `domicilio` varchar(50) COLLATE utf8mb4_spanish_ci NOT NULL,
-  `tel_movil` int NOT NULL,
-  `tel_fijo` int NOT NULL,
-  `email` varchar(250) COLLATE utf8mb4_spanish_ci NOT NULL,
-  `creado` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+DROP TABLE IF EXISTS `raza`;
+CREATE TABLE IF NOT EXISTS `raza` (
+  `id` int(11) NOT NULL,
+  `id_especie` int(11) NOT NULL,
+  `raza` varchar(50) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `id` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `tipo`
---
-
-DROP TABLE IF EXISTS `tipo`;
-CREATE TABLE IF NOT EXISTS `tipo` (
-  `id` int NOT NULL,
-  `tipo` varchar(50) COLLATE utf8mb4_spanish_ci NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `id` (`id`)
+  KEY `especie` (`id_especie`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 
 --
@@ -74,10 +115,24 @@ CREATE TABLE IF NOT EXISTS `tipo` (
 --
 
 --
+-- Filtros para la tabla `historia_clinica`
+--
+ALTER TABLE `historia_clinica`
+  ADD CONSTRAINT `historia_clinica_ibfk_1` FOREIGN KEY (`id_mascota`) REFERENCES `mascota` (`id`);
+
+--
 -- Filtros para la tabla `mascota`
 --
 ALTER TABLE `mascota`
-  ADD CONSTRAINT `mascota_ibfk_1` FOREIGN KEY (`tipo`) REFERENCES `tipo` (`id`) ON DELETE RESTRICT;
+  ADD CONSTRAINT `mascota_ibfk_1` FOREIGN KEY (`id_especie`) REFERENCES `especie` (`id`),
+  ADD CONSTRAINT `mascota_ibfk_2` FOREIGN KEY (`raza`) REFERENCES `raza` (`id`),
+  ADD CONSTRAINT `mascota_ibfk_3` FOREIGN KEY (`id_cliente`) REFERENCES `cliente` (`id`);
+
+--
+-- Filtros para la tabla `raza`
+--
+ALTER TABLE `raza`
+  ADD CONSTRAINT `raza_ibfk_1` FOREIGN KEY (`id_especie`) REFERENCES `especie` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
